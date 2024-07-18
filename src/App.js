47a7dd6d0d239954df7de33dd35cab539a9d3ab0
dev.dong9897 <div.dong9897@gmail.com>
2024-07-18 20:35:45 +0900

    정렬기능완료

--- src/App.js
+++ src/App.js
@@ -5,21 +5,29 @@ import ReviewList from "./components/ReviewList";
 
 function App() {
   const [items, setItems] = useState([]);
+  const [order, setOrder] = useState("createdAt");
 
   useEffect(() => {
-    handleLoad();
-  }, []);
+    handleLoad({ order });
+  }, [order]);
 
-  const handleLoad = async () => {
-    const { reviews } = await getReviewApi();
+  const handleLoad = async (option) => {
+    const { reviews } = await getReviewApi(option);
     console.log(reviews);
     setItems(reviews);
   };
+
+  const handleOrderClick = () => setOrder("createdAt");
+  const handleBestClick = () => setOrder("rating");
   return (
     <div className="App">
       <div className="order-button">
-        <button className="order">최신순</button>
-        <button className="order">베스트순</button>
+        <button className="order" onClick={handleOrderClick}>
+          최신순
+        </button>
+        <button className="order" onClick={handleBestClick}>
+          베스트순
+        </button>
       </div>
       <ReviewList items={items} />
     </div>
--- src/components/api.js
+++ src/components/api.js
@@ -1,5 +1,8 @@
-export async function getReviewApi() {
-  const response = await fetch("https://learn.codeit.kr/api/film-reviews");
+export async function getReviewApi({ order = "createdAt" }) {
+  const query = `order=${order}`;
+  const response = await fetch(
+    `https://learn.codeit.kr/api/film-reviews?${query}`
+  );
   const data = await response.json();
   return data;
 }
